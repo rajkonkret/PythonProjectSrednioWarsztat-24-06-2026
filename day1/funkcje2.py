2de4f935363e80205491ce18316b1cd187bae66c
rjkonkret <rajkonkret660@gmail.com>
2026-06-24 15:24:25 +0200

    [1 21] funkcje2.py.py

--- day1/funkcje2.py
+++ day1/funkcje2.py
@@ -32,3 +32,29 @@ print(odczyt_pliku())  # Odczytane dane z pliku...
 
 zapis_pliku = plik("zapis")
 zapis_pliku()  # Zapisałem dane do pliku...
+
+
+# domknięcie
+# funkcja wewnętrzna pamięta wartości przekazane w funkcji zewnętrznej
+def obsluga_zamowienia(numer_zamowienia):
+    status = "nowe"
+
+    def zmien_status(nowy_status):
+        # status
+        nonlocal status
+        poprzedni_status = status
+        status = nowy_status
+
+        print(f"""
+Zamówienie {numer_zamowienia}:
+{poprzedni_status} -> {status}
+""")
+
+    return zmien_status
+
+zamowinie_1001 = obsluga_zamowienia(1001)
+
+zamowinie_1001("opłacone")
+zamowinie_1001("spakowane")
+zamowinie_1001("wysłane")
+zamowinie_1001("dostarczone")
