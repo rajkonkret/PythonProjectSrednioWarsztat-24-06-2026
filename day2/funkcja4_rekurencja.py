ca6921d37f0e1f1d06a0d29c523dc82dcbb429a6
rjkonkret <rajkonkret660@gmail.com>
2026-06-25 10:51:07 +0200

    [2 10] add funkcja4_rekurencja.py

--- day2/funkcja4_rekurencja.py
+++ day2/funkcja4_rekurencja.py
@@ -34,3 +34,31 @@ print(fibonacci.cache_info())
 print(fibonacci(10))  # 55
 print(fibonacci.cache_info())
 # CacheInfo(hits=9, misses=11, maxsize=512, currsize=11)
+
+# zsumowac wszystkie wartosci ze słownika
+nested_data = {
+    "a": 1,
+    "b": {
+        "c": 2,
+        "d": [3, 4, {"e": 5}]
+    },
+    "f": [6, 7]
+}
+
+
+def sum_nested(data):
+    total = 0
+
+    if isinstance(data, dict):
+        for value in data.values():
+            total += sum_nested(value)
+    elif isinstance(data, list):
+        for item in data:
+            total += sum_nested(item)
+    elif isinstance(data, (int, float)):
+        total += data
+
+    return total
+
+result = sum_nested(nested_data)
+print(result) # 28
