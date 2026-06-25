d8b8224764b8647a4833ba1564b51fe3843c775b
rjkonkret <rajkonkret660@gmail.com>
2026-06-25 12:13:19 +0200

    [2 19] add funkcja5_dekorator.py

---
+++ day2/funkcja8_np.py
@@ -0,0 +1,8 @@
+import numpy as np
+import tracemalloc
+
+lista1 = list(range(10_000_000))
+lista2 = list(range(10_000_000))
+
+array1 = np.arange(10_000_000, dtype=np.int64)
+array2 = np.arange(10_000_000, dtype=np.int64)
