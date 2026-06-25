22d2ef19d946edb3ac3b79023020bb0248f25d62
rjkonkret <rajkonkret660@gmail.com>
2026-06-25 09:43:45 +0200

    [1 25] add funkcja3

--- day1/funkcje3.py
+++ day1/funkcje3.py
@@ -43,3 +43,38 @@ print(f"Lambda: {list(map(lambda cena: round(cena * 1.23, 2), ceny_netto))}")
 # Lambda: [123.0, 307.5, 98.4, 1476.0, 55.35]
 
 print(f"Lambda: {map(lambda cena: round(cena * 1.23, 2), ceny_netto)}")  # Lambda: <map object at 0x000001D49D777B80>
+
+# filtrowanie
+temperatury = [-12, -2, 0, 4, 15, 22, 31, 38, 38]
+
+# wyfiltrowac do nowej listy, większe niż 30
+upalne_dni = []
+
+for t in temperatury:
+    if t >= 30:
+        upalne_dni.append(t)
+
+print(upalne_dni)  # [31, 38, 38]
+
+upalne_dni_2 = [temp for temp in temperatury if temp > 30]
+print(upalne_dni_2)  # [31, 38, 38]
+
+liczby = [1, 2, 3, 4, 5]
+
+wynik = ["parzysta" if liczba % 2 == 0 else "nieparzysta" for liczba in liczby]
+print(wynik)  # ['nieparzysta', 'parzysta', 'nieparzysta', 'parzysta', 'nieparzysta']
+
+# filter()
+upalne_dni_3 = list(
+    filter(lambda t: t >= 30, temperatury)
+)
+
+print(upalne_dni_3)  # [31, 38, 38]
+
+# > 10 , < 25
+umiarkowane = list(
+    # filter(lambda t: t > 10 and t < 25, temperatury)
+    filter(lambda t: 10 < t < 25, temperatury)
+)
+print(umiarkowane)  # [15, 22]
+# ctrl alt l
