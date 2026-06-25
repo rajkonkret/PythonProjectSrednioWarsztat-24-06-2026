b4a80be1bc5dcec7965072b2d43e33db77501252
rjkonkret <rajkonkret660@gmail.com>
2026-06-25 10:43:35 +0200

    [2 9] add funkcja4_rekurencja.py

--- day2/funkcja4_rekurencja.py
+++ day2/funkcja4_rekurencja.py
@@ -1,5 +1,7 @@
 # funkcja rekurencyjna
 # funkcja wywołuja sama siebie
+from functools import lru_cache
+
 
 def silnia(n):
     if n == 0:
@@ -9,3 +11,26 @@ def silnia(n):
 
 
 print(silnia(5))  # 120
+
+
+@lru_cache(maxsize=512)
+def fibonacci(n):
+    if n < 0:
+        raise ValueError("n nie może byc ujemne")
+
+    if n < 2:
+        return n
+
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+print(fibonacci.cache_info())
+# CacheInfo(hits=0, misses=0, maxsize=512, currsize=0)
+print(fibonacci(5))  # 55
+print(fibonacci.cache_info())
+
+print(fibonacci.cache_info())
+# CacheInfo(hits=3, misses=6, maxsize=512, currsize=6)
+print(fibonacci(10))  # 55
+print(fibonacci.cache_info())
+# CacheInfo(hits=9, misses=11, maxsize=512, currsize=11)
