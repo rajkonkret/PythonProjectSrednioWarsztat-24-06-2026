9c720f4bbf14e7931bb4f57b9bd88b5844725959
rjkonkret <rajkonkret660@gmail.com>
2026-06-25 13:36:08 +0200

    [2 25] add funkcja10_gen2.py

--- day2/funkcja10_gen2.py
+++ day2/funkcja10_gen2.py
@@ -11,4 +11,16 @@ def wznowienie(n, k):
     print("Krok ostatni")
     yield n * k
 
+
 print(20 * "-")
+for i in wznowienie(6, 8):
+    if i == 1001:
+        continue
+    time.sleep(1)
+    print(f"Yield zwraca wartości: {i}")
+# --------------------
+# Wstrzymanie diałania...
+# Kolejny krok
+# Yield zwraca wartości: 14
+# Krok ostatni
+# Yield zwraca wartości: 48
