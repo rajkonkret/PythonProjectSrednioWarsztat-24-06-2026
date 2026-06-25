762ef9ec2a243d5222b47af67570188ab451c43a
rjkonkret <rajkonkret660@gmail.com>
2026-06-25 11:42:33 +0200

    [2 14] add funkcja5_dekorator.py

--- day2/funkcja6_dek2.py
+++ day2/funkcja6_dek2.py
@@ -18,3 +18,33 @@ def nasza_funkcja(a, b, c):
 
 
 print(nasza_funkcja(1, 2, c=90))
+
+# https://en.wikipedia.org/wiki/ANSI_escape_code
+print("\033[31mHello\033[0m")  # kolorki
+
+# pip install colorama
+from colorama import Fore, Style, init
+
+init(autoreset=True)
+
+
+def czerwony_bold(func):
+    def wrapper(*args, **kwargs):
+        wynik = func(*args, **kwargs)
+
+        print(
+            Style.BRIGHT + Fore.RED + str(wynik) + Style.RESET_ALL
+        )
+        return wynik
+
+    return wrapper
+
+
+@czerwony_bold
+def komunikat():
+    return "Uwaga! Wystąpił bład."
+
+
+print(komunikat())
+# Uwaga! Wystąpił bład.
+# Uwaga! Wystąpił bład.
