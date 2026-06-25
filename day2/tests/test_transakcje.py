9e2e5822b55ce100c0f6df44dca49ebd0c9c88d0
rjkonkret <rajkonkret660@gmail.com>
2026-06-25 15:15:33 +0200

    [2 34] add test_transakcje.py

--- day2/tests/test_transakcje.py
+++ day2/tests/test_transakcje.py
@@ -22,5 +22,19 @@ def test_map_transactions_usd():
 
 def test_reduce_transactions():
     assert tr.reduce_transactions([1000, 500, 700, 0]) == 2200
+
+
 # day2/tests/test_transakcje.py::test_map_transactions_usd PASSED          [ 50%]
 # day2/tests/test_transakcje.py::test_reduce_transactions PASSED           [100%]
+
+
+def test_filter_transactions_income():
+    expected_list = [
+        {'id': 1, "type": "income", "amount": 1000, "currency": "USD"},
+
+        {'id': 3, "type": "income", "amount": 500, "currency": "USD"},
+
+        {'id': 5, "type": "income", "amount": 700, "currency": "USD"},
+
+        {'id': 7, "type": "income", "amount": 100, "currency": "EUR"},
+    ]
