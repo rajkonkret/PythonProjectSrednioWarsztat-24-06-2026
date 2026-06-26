11883934113dce604014dd0ba2860a581ae84889
rjkonkret <rajkonkret660@gmail.com>
2026-06-26 11:25:28 +0200

    [2 49] add kl4

--- day3/kl4_missing.py
+++ day3/kl4_missing.py
@@ -13,4 +13,20 @@ print(d1)  # {}
 print(d1['name'])  # default
 
 d2 = {}
+
+
 # print(d2['name'])  # KeyError: 'name'
+
+# słownik, który gdy nie ma klucza tworzy taki klucz z wartością domyslna
+class AutoDict(dict):
+    def __missing__(self, key):
+        # slownik['name'] = 'Radek'
+        self[key] = 0
+        return key
+
+
+a1 = AutoDict()
+print(a1)  # {}
+print(a1['name'])
+print(a1)
+# {'name': 0}
